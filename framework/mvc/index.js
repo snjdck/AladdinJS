76cd8934a58b980b486324f0613012dd4dfa4746
Alex Shao <shao_kai@qq.com>
2018-06-20 18:48:49 +0800

    update

--- framework/mvc/index.js
+++ framework/mvc/index.js
@@ -1,6 +1,13 @@
 
-export * from './src/Application';
-export * from './src/Module';
+import Application from './src/Application';
+import Module from './src/Module'
+import Msg from './src/Msg';
+
+export {
+	Application,
+	Module,
+	Msg
+};
 export * from './src/Msg';
 export * from './src/Notifier';
 export * from './src/react';
