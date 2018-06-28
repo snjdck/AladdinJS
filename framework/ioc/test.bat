12ba9aa23f06072413dad60a85e4e7e20b8ae925
Alex Shao <shao_kai@qq.com>
2018-06-28 15:58:24 +0800

    update

---
+++ framework/ioc/test.bat
@@ -0,0 +1,2 @@
+@node --harmony --harmony-class-fields --harmony-static-fields --experimental-modules ./test
+@pause
---
+++ framework/ioc/test/index.mjs
@@ -0,0 +1,5 @@
+setInterval(function(){
+	console.log('init')
+}, 1000)
+console.log('init')
+
